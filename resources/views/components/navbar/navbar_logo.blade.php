cb94e81455b5f049304feae9990eee6c6663ece4
Alessandro Geras <alessandrogeras@gmail.com>
2022-08-01 18:30:38 -0300

    php - index.php - menu

---
+++ resources/views/components/navbar/navbar_logo.blade.php
@@ -0,0 +1,3 @@
+<div id="navbar_logo">
+  <img src="https://videos.adventistas.org/pt/wp-content/themes/pa-theme-sedes/assets/sedes/pt/default.svg">
+</div>
---
+++ resources/views/components/navbar/panel.blade.php
@@ -0,0 +1,10 @@
+<div id="panel">
+  <ul>
+    <li><a class="menu_open" id="menu_open" href="#" onclick="menu_click(this.id)">
+        <x-heroicon-o-menu />
+      </a></li>
+    <li><a class="menu_close" id="menu_close" href="#" onclick="menu_click(this.id)">
+        <x-heroicon-o-x />
+      </a></li>
+  </ul>
+</div>
