aee601f6a81441dc996c6296baf0ebb4fdbd54a5
Alessandro Geras <alessandrogeras@gmail.com>
2022-08-08 13:52:41 -0300

    php - panel.php - alterado ícones do painel

--- resources/views/components/navbar/panel.blade.php
+++ resources/views/components/navbar/panel.blade.php
@@ -1,10 +1,10 @@
 <div id="panel">
   <ul>
     <li><a class="menu_open" id="menu_open" href="#" onclick="menu_click(this.id)">
-        <x-heroicon-o-menu />
+        &#9776
       </a></li>
     <li><a class="menu_close" id="menu_close" href="#" onclick="menu_click(this.id)">
-        <x-heroicon-o-x />
+        &#x2715
       </a></li>
   </ul>
 </div>
