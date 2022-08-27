cc7000adb40e6c53644c33bf89528cbe934677bb
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-08-27 00:17:12 -0300

    php - Controller - Criada funções de autenticação

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
     public function index()
     {
         return response()->view('index')->setStatusCode(200);
+    }
+
+    public function login()
+    {
+        return response()->view('login')->setStatusCode(200);
+    }
+
+    public function autorizar(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['email'],
+            'password' => ['password'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) { 
+            return redirect()->intended();
+        }
+        else{
+            return back()->with("msg", "Erro de autenticação: Verifique seu email e a senha")->setStatusCode(401);
+        }
     }
 }
