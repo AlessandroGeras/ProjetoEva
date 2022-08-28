d7f4066911e3caaa11927d027d3177c160ad2fc4
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-08-28 00:22:55 -0300

    php - routes - Atualizado nome das rotas

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,15 @@ use App\Http\Controllers\UsersController;
 //Rotas de Autenticação
 Route::get('/auth/login', [UsersController::class, "login"])->name('login');
 
-Route::post('/autenticar', [UsersController::class, "autorizar"]);
+Route::post('/autenticar', [UsersController::class, "autorizar"])->name('autenticar');
 
 //Home
 Route::get('/', [UsersController::class, "index"]);
 
 //Rotas das Palestras
-Route::get('/palestras', [PalestrasController::class, "show"])->middleware('auth');
+Route::get('/palestras', [PalestrasController::class, "show"])->name('palestras')->middleware('auth');
 
-Route::post('/criar_palestra', [PalestrasController::class, "store"]);
+Route::post('/criar_palestra', [PalestrasController::class, "store"])->name('criar_palestra');
 
 Route::get('/palestras/{id}', [PalestrasController::class, "palestra"]);
 
