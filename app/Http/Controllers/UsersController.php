b4eb137fa5b061d9b03a05bdebb459e5ecc3563a
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-08-30 14:19:50 -0300

    UsersController - Bibliotecas foram atualizadas

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class UsersController extends Controller
 {
