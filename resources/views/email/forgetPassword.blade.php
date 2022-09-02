bf5a9e434f284bc31e7719425b4c8969e07e9000
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-09-02 05:16:01 -0300

    php - forgetPassword - Atualizada rota

--- resources/views/email/forgetPassword.blade.php
+++ resources/views/email/forgetPassword.blade.php
@@ -1,4 +1,4 @@
 <h1>Email de recuperação de senha</h1>
    
 Você pode resetar sua senha no site Projeto Eva clicando no link abaixo:
-<a href="{{ route('reset.password.get', $token) }}">Resetar Senha</a> 
+<a href="{{ route('resetPassword', $token) }}">Resetar Senha</a> 
