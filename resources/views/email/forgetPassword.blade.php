f5cc3b4b0f3d2b979314f4430c0c74e540b9b606
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-09-02 05:07:36 -0300

    Criado email de recuperação de senha

---
+++ resources/views/email/forgetPassword.blade.php
@@ -0,0 +1,4 @@
+<h1>Email de recuperação de senha</h1>
+   
+Você pode resetar sua senha no site Projeto Eva clicando no link abaixo:
+<a href="{{ route('reset.password.get', $token) }}">Resetar Senha</a> 
