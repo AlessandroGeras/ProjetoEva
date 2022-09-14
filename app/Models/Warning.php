dcd967f88c4922f0996ea11165ac5e8ea914212b
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-09-14 18:21:13 -0300

    php - warning.php -Spatie removido

--- app/Models/Warning.php
+++ app/Models/Warning.php
@@ -9,7 +9,6 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 
 
 class Warning extends Authenticatable
@@ -19,7 +18,6 @@ class Warning extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
-    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
