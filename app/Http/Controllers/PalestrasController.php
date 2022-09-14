46bd77b8497913ec4598f724d9a6828d8bd90b04
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-09-14 19:29:32 -0300

    Commit de teste de permissão

--- app/Http/Controllers/PalestrasController.php
+++ app/Http/Controllers/PalestrasController.php
@@ -70,6 +70,8 @@ class PalestrasController extends Controller
         $manypalestras = $user->palestras;
 
         $manyusers = $palestra->users;
+        
+        dd($user->permission->permission);
 
         return view('palestra', ["palestra" => $palestra,"months"=>$months,"manypalestras"=>$manypalestras,"manyusers"=>$manyusers, "user"=>$user]);
     }
