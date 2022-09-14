e3df697a39033af078ba5deef73ae779dd3b3edb
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-09-14 19:11:51 -0300

    PalestrasController - Adicionada biblioteca Auth

--- app/Http/Controllers/PalestrasController.php
+++ app/Http/Controllers/PalestrasController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use GuzzleHttp\Client;
 use Illuminate\Console\Scheduling\Event;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 
 date_default_timezone_set('America/Sao_Paulo');
