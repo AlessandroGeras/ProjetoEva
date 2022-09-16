042f73ca8acf6958ee2ad0d61347b17c09ed3641
Vinícius Jung <94872696+Vinnie-Jung@users.noreply.github.com>
2022-09-16 14:44:09 -0300

    Dashboard - Adicionando carrosel (admin/profissional)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,13 @@
 <x-dashboard.my_account :user="$user" />
 
 @if($user->permission->permission==('Usuário'))
-<x-dashboard.my_events :manypalestras="$manypalestras" />
+<x-dashboard.my_events :manypalestras="$manypalestras"/>
 
 <x-dashboard.notifications/>
+@endif
+
+@if($user->permission->permission==('Administrador')||('Profissional'))
+<x-dashboard.admin_palestras :palestras="$palestras" />
 @endif
 
 @endsection
