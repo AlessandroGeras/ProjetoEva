82b665bb4a71e2f96b45ebd8f7f2134aa266e4a3
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-09-20 17:49:14 -0300

    UsersController - Adicionada biblioteca de email

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 date_default_timezone_set('America/Sao_Paulo');
 
