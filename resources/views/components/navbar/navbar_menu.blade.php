2e2b8c74384150eca73d77b72dcbbbab78a17814
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-09-25 15:58:30 -0300

    php - navbar_menu.php - Adicionado font serif

--- resources/views/components/navbar/navbar_menu.blade.php
+++ resources/views/components/navbar/navbar_menu.blade.php
@@ -1,4 +1,4 @@
-<div id="navbar_menu" class="mobile_close w-full h-full block bg-blue-500 float-left lg:float-right lg:w-2/3 lg:bg-transparent xl:w-1/2">
+<div id="navbar_menu" class="mobile_close w-full h-full font-serif block bg-blue-500 float-left lg:float-right lg:w-2/3 lg:bg-transparent xl:w-1/2">
   <ul class="font-bold center text-center leading-none lg:flex">
     <li><a class="link" href="{{ route('profissionais')}}">PROFISSIONAIS</a></li>
     <li><a class="link" href="{{ route('eva')}}">PROJETO EVA</a></li>
