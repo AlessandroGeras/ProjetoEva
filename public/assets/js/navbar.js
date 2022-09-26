440bea9909a767b6b8af5365ad95d7774b7cd504
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-09-26 01:42:28 -0300

    js - navbar.js - Largura da navbar aumentada

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -4,7 +4,7 @@ const menu_click = (clicked_id) => {
 
     if (clicked_id == "menu_open") {
         let navbar_menu = document.getElementById("navbar_menu");
-        navbar_menu.className = "mobile_open w-full h-full block bg-blue-700 float-left lg:float-right lg:w-2/3 lg:bg-transparent xl:w-1/2";
+        navbar_menu.className = "mobile_open w-full h-full block bg-blue-500 float-left font-serif lg:float-right lg:w-3/4 lg:bg-transparent xl:w-3/5";
 
         let menu_open = document.getElementById("menu_open");
         menu_open.className = "menu_close hidden";
@@ -26,7 +26,7 @@ const menu_click = (clicked_id) => {
     else {
 
         let navbar_menu = document.getElementById("navbar_menu");
-        navbar_menu.className = "mobile_close w-full h-full block bg-blue-700 float-left lg:float-right lg:w-2/3 lg:bg-transparent xl:w-1/2";
+        navbar_menu.className = "mobile_close w-full h-full block bg-blue-500 float-left font-serif lg:float-right lg:w-3/4 lg:bg-transparent xl:w-3/5";
 
         let menu_open = document.getElementById("menu_open");
         menu_open.className = "menu_open";
