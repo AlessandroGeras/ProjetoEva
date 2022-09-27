a2a92c4ed9440409360692f1d4d4026956d3454b
Eliabe Ribeiro Mota <100526198+Eliabe-Ribeiro-22@users.noreply.github.com>
2022-09-27 17:17:03 -0300

    Implementação Tailwind, responsividade e dark mode

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -4,17 +4,17 @@ const menu_click = (clicked_id) => {
 
     if (clicked_id == "menu_open") {
         let navbar_menu = document.getElementById("navbar_menu");
-        navbar_menu.className = "mobile_open w-full h-full block bg-blue-500 float-left font-serif lg:float-right lg:w-3/4 lg:bg-transparent xl:w-3/5";
+        navbar_menu.className = "mobile_open w-full h-full block bg-blue-500 float-left lg:float-right lg:w-3/4 lg:bg-transparent xl:w-3/5";
 
         let menu_open = document.getElementById("menu_open");
-        menu_open.className = "menu_close hidden";
+        menu_open.className = "menu_close hidden dark:text-gray-300";
 
         let menu_close = document.getElementById("menu_close");
-        menu_close.className = "menu_open";
+        menu_close.className = "menu_open dark:text-gray-300";
         
         let index_logo = document.getElementById("index_logo");
             if (index_logo) {
-            index_logo.className = "margin";
+            index_logo.className = "inline-block mt-0 w-full h-36 bg-black lg:mt-0 sm:h-48 dark:bg-gray-700";
             } 
         
         let cursos_title = document.getElementById("palestras_title");
@@ -26,17 +26,17 @@ const menu_click = (clicked_id) => {
     else {
 
         let navbar_menu = document.getElementById("navbar_menu");
-        navbar_menu.className = "mobile_close w-full h-full block bg-blue-500 float-left font-serif lg:float-right lg:w-3/4 lg:bg-transparent xl:w-3/5";
+        navbar_menu.className = "mobile_close w-full h-full block bg-blue-500 float-left lg:float-right lg:w-3/4 lg:bg-transparent xl:w-3/5";
 
         let menu_open = document.getElementById("menu_open");
-        menu_open.className = "menu_open";
+        menu_open.className = "menu_open dark:text-gray-300";
 
         let menu_close = document.getElementById("menu_close");
-        menu_close.className = "menu_close hidden";
+        menu_close.className = "menu_close hidden dark:text-gray-300";
         
         let index_logo = document.getElementById("index_logo");
             if (index_logo) {
-                index_logo.className = "no_margin";
+                index_logo.className = "inline-block -mt-24 w-full h-36 bg-black lg:mt-0 sm:h-48 dark:bg-gray-700";
             }
 
         let cursos_title = document.getElementById("palestras_title");
--- resources/views/components/index/index_logo.blade.php
+++ resources/views/components/index/index_logo.blade.php
@@ -1,14 +1,16 @@
-<div id="index_logo" class="index_logo_nomargin">
-    <div id="index_login">
-        <h1>Bem vindo ao projeto Eva</h1>  
+<html>
+<div id="index_logo" class="inline-block -mt-24 w-full h-36 bg-black lg:mt-0 sm:h-48 dark:bg-gray-700">
+    <div id="index_login" class="block m-auto center">
+        <div class="h4 text-center text-white sm:text-[56px]">Bem vindo ao projeto Eva</div> 
 
         @guest
-        <div id="login_link">
-            <a href="{{ route('login') }}">
-                <h3>LOGIN</h3>
+        <div class="flex m-auto w-20 text-black p-2 sm:p-4">
+            <a class="mx-auto block text-black bg-white"href="{{route('login')}}">
+                <div class="text-black text-xs p-1 sm:p-2 sm:text-base">LOGIN</div>
             </a>
         </div>
         @endguest
 
     </div>
 </div>
+</html>
