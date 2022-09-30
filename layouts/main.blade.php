eaa737c2f9e5bac7cd6d62fbd436b3b04adc5c0c
Eliabe Ribeiro Mota <100526198+Eliabe-Ribeiro-22@users.noreply.github.com>
2022-09-30 16:33:44 -0300

    php - Refatoração final da Navbar - Início da Sprint de testes.

---
+++ layouts/main.blade.php
@@ -0,0 +1,34 @@
+<?php date_default_timezone_set('America/Sao_Paulo'); ?>
+
+<html>
+
+<head>
+  <title>@yield("title")</title>
+
+  <x-layouts.head />
+</head>
+
+<body class="bg-gray-200 dark:bg-gray-900">
+  @if(session('msg'))
+  <script>
+    message("<?php echo session('msg') ?>")
+  </script>
+  @endif
+
+  <navbar>
+    <div class="h-24 dark:bg-black">
+      <x-navbar.navbar_logo />
+
+      <x-navbar.navbar_panel />
+
+      <x-navbar.navbar_menu />
+    </div>   
+  </navbar>
+
+  @yield("corpo")
+  
+  <x-navbar.footer />
+
+</body>
+
+</html>
