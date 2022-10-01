9099e6a9288a98fcebd4de43ac43ae8267cc169d
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-10-01 02:23:38 -0300

    css - navbar_menu - Largurada alterada da Navbar

--- resources/views/components/navbar/navbar_menu.blade.php
+++ resources/views/components/navbar/navbar_menu.blade.php
@@ -1,4 +1,4 @@
-<div id="navbar_menu" class="mobile_close w-full h-full block bg-blue-500 float-left lg:float-right lg:w-[650px] lg:bg-transparent">
+<div id="navbar_menu" class="mobile_close w-full h-full block bg-blue-500 float-left lg:float-right lg:w-[700px] lg:bg-transparent">
   <ul class="font-bold center text-center justify-center leading-none lg:flex lg:dark:text-white">
     <li><a class="link" href="{{ route('profissionais')}}">PROFISSIONAIS</a></li>
     <li><a class="link" href="{{ route('eva')}}">PROJETO EVA</a></li>
@@ -14,7 +14,7 @@
     <li>
       <form action="{{ route('logout')}}" method="POST">
         @csrf
-        <a class="link" onclick="event.preventDefault();this.closest('form').submit();">LOGOUT</a>
+        <a class="link lg:mt-12" onclick="event.preventDefault();this.closest('form').submit();">LOGOUT</a>
       </form>
     </li>
     @endauth
