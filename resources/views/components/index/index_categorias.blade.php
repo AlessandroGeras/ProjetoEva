a0bb6a70dd0d93283f089c4664b6644a060a676f
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-10-03 04:14:30 -0300

    index_categorias - Cores alteradas do darkmode

--- resources/views/components/index/index_categorias.blade.php
+++ resources/views/components/index/index_categorias.blade.php
@@ -1,14 +1,14 @@
 <div id="index_categorias" class="w-5/6 h-48 mx-auto mt-12 mb-10 grid gap-5 grid-cols-1 lg:grid-cols-2">
     <div class="w-full h-full bg-[url('/assets/img/site/instituicao.png')] bg-center bg-cover">
-        <div class="text-white font-sans font-bold text-xl flex center justify-center sm:text-4xl">CONHEÇA O EVA</div>
+        <div class="text-white font-sans font-bold text-xl flex center justify-center sm:text-4xl dark:text-gray-200">CONHEÇA O EVA</div>
     </div>
 
     <div class="w-full h-full bg-[url('/assets/img/site/palestras.jpeg')] bg-center bg-cover">
-        <div class="text-white font-sans font-bold text-xl flex center justify-center sm:text-4xl"><a href="{{route('palestras')}}">PALESTRAS</a></div>
+        <div class="text-white font-sans font-bold text-xl flex center justify-center sm:text-4xl dark:text-gray-200"><a href="{{route('lectures')}}">PALESTRAS</a></div>
     </div>
 </div>
 
-<div class="h5 bg-zinc-900 text-white text-center p-0.5 mb-10 dark:bg-sky-900">Vídeos Institucionais</div>
+<div class="h5 bg-zinc-900 text-white text-center p-0.5 mb-10 dark:bg-sky-900 dark:text-gray-200">Vídeos Institucionais</div>
 <div class="w-5/6 h-48 mx-auto mb-12 grid gap-5 grid-cols-1 sm:grid-cols-2">    
     <div class="w-44 h-24 sm:w-56 sm:h-36 lg:w-96 lg:h-48 mx-auto"> <iframe class="w-44 h-24 sm:w-56 sm:h-36 lg:w-96 lg:h-48" src="https://www.youtube.com/embed/Nmf60ObHhzo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
 
