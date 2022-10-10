e536d7afbc001d04842e98453bfb633d09a46dae
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-10-09 23:00:56 -0300

    js - darkmode.js - Localstorage implementado

--- public/assets/js/darkmode.js
+++ public/assets/js/darkmode.js
@@ -3,8 +3,10 @@ const darkmode = () => {
     for (var i = 0; i < elementArray.length; i++) {
         if (elementArray[i].className == "dark") {
             elementArray[i].className = "";
+            localStorage.setItem("darkmode", "off");
         } else {
             elementArray[i].className = "dark";
+            localStorage.setItem("darkmode", "on");
         }
     }
 };
