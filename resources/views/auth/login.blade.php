68f821677eb346c8e9dfec13a75c5dd7bb6e275d
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-10-12 13:42:02 -0300

    php - login.php - Rota nomeada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                <form method="POST" action="{{route('autenticar')}}">
+                <form method="POST" action="{{route('auth')}}">
                     @csrf
                     <div>
                         <label class="block font-medium text-sm text-gray-700" for="email">
