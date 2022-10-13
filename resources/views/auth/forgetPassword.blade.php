8926857e179bf25c8312e8642daed48689c59cb4
Eliabe Ribeiro Mota <100526198+Eliabe-Ribeiro-22@users.noreply.github.com>
2022-10-12 21:03:34 -0300

    php - forgetPassword - Resolução de bug em route dessa view

--- resources/views/auth/forgetPassword.blade.php
+++ resources/views/auth/forgetPassword.blade.php
@@ -14,7 +14,7 @@
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                <form method="POST" action="{{route('submitForgetPasswordForm')}}">
+                <form method="POST" action="{{ route('submit-forget-password')}}">
                     @csrf
                     <div>
                         <label class="block font-medium text-sm text-gray-700" for="email">
