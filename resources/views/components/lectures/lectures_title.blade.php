6f6ade6aa5a8bed4adb5627a7a45674037a0d503
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-10-13 17:29:03 -0300

    php - lectures_title.php - Código refatorado

--- resources/views/components/lectures/lectures_title.blade.php
+++ resources/views/components/lectures/lectures_title.blade.php
@@ -1,12 +1,12 @@
 <html>
 <div id="lectures_title" class="-mt-24 inline-block w-full lg:mt-0">
     {{$slot}}
-    <div class="w-full sm:w-[450px] h-24 mx-auto my-6 lecture dark:lectureDarkmode">
+    <div class="w-full sm:w-[450px] h-24 mx-auto my-6 gradient_panel dark:gradient_panel_darkmode">
         <div class="text-xl text-center dark:text-[#505739]">Palestras</div>
-        <div class="styled_input dark:styled_inputDarkmode">
+        <div class="styled_input dark:styled_input_darkmode">
             <form class="w-full sm:ml-[7.5%]" action="{{route('lectures')}}" method="GET" onsubmit="loading('Pesquisando palestras')">
                 @csrf
-                <input class="styled_warning dark:styled_warningDarkmode" type="text" name="search" placeholder="Digite o nome da palestra"></input>
+                <input class="styled_warning dark:styled_warning_darkmode" type="text" name="search" placeholder="Digite o nome da palestra"></input>
                 <button type="submit" class="styled_warning"> &#10132;</button>
             </form>
         </div>
